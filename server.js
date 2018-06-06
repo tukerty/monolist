2bc65bb40ccc87d0ead1ec45ab70e9a99f53342c
Tim Freeman <tukerty@Gmail.com>
2018-06-06 20:24:20 +0300

    initial

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static('dist'))
+
+//КОД ПОТЯША
+
+var server = app.listen(8080, function () {
+    var host = "0.0.0.0"
+    var port = "8080"
+    
+    console.log("Example app listening at http://%s:%s", host, port)
+ })
