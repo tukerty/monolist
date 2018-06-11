dce026b25eee72aef0ff6f4bb288492fc78522a9
ptsh <atah-heroes@yandex.ru>
2018-06-11 11:06:03 +0500

    merge

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ app.use(express.static('dist'))
 //КОД ПОТЯША Moi code
 var a = "sosipisos"
 
-var server = app.listen(8080, function () {
+var server = app.listen(3000, function () {
     var host = "0.0.0.0"
-    var port = "8080"
+    var port = "3000"
     
     console.log("Example app listening at http://%s:%s", host, port)
  })
